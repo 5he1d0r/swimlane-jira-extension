28805b79bc3e912e83999deb59e6d34903760098
Sebastian Linde <5he11d000r@gmail.com>
2023-05-26 21:11:27 +0200

    initial user-script

---
+++ swimlane-jira-extension.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Jira Assignee on swimlane
+// @namespace    jira
+// @version      0.1
+// @description  display the assignee on closed swimlane
+// @author       5he1d0r
+// @match        *://*/*RapidBoard.jspa?rapidView=*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+// TODO: propper styling of the avatars
+const avatar = (assignee) =>
+`
+<span class="aui-avatar">
+  <span class="aui-avatar-inner">
+    <img src="${assignee.avatarUrls["48x48"]}" class="aui-avatar-img" title="Bearbeiter: ${assignee.displayName}" loading="lazy" alt="${assignee.name}" />
+  </span>
+</span>
+`
+
+const displayAssignee = (element, json) => {
+    let assignee = json.fields.assignee
+    if(assignee == null) return;
+    let e = element.getElementsByClassName("ghx-bandaid")[0];
+    e.innerHTML = e.innerHTML + avatar(assignee);
+}
+
+const loadTicketAndModifySwimlane = async (element) => {
+    let ticket = element.getAttribute("data-issue-key");
+    if(ticket == null) return;
+    const response = await fetch(`${window.location.origin}/rest/api/latest/issue/${ticket}`);
+    let json = await response.json();
+    displayAssignee(element, json);
+}
+
+const performJiraExtension = () => {
+    let y = window.document.getElementsByClassName("ghx-swimlane-header");
+    Array.prototype.forEach.call(y, element => {
+        loadTicketAndModifySwimlane(element);
+    });
+};
+
+(function() {
+    setTimeout(performJiraExtension, 5000);
+})();
