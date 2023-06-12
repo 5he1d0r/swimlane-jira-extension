3aa2d6db52ac45d81c4e7537eb3b7d9373a2f396
Linde, Sebastian <Sebastian.Linde@adesso.de>
2023-06-12 19:57:36 +0200

    add gitignore and impl auto-update of avatars on reload

--- swimlane-jira-extension.user.js
+++ swimlane-jira-extension.user.js
@@ -2,7 +2,7 @@
 // @name         swimlane jira extension
 // @namespace    https://openuserjs.org/users/5he1d0r
 // @copyright    2023, 5he1d0r (https://openuserjs.org/users/5he1d0r)
-// @version      0.1
+// @version      0.2.0
 // @license      MIT
 // @author       5he1d0r
 // @match        *://*/*RapidBoard.jspa?rapidView=*
@@ -16,17 +16,26 @@
 // @author      5he1d0r
 // ==/OpenUserJS==
 
+// changelog
+//
+// 0.2.0 fix reload issue
+// * checks every 2 seconds if there is missing a specific css-class
+// * 5-second delay after the last update
+// * not perfect response times on change
+//
+// 0.1 Initial release
+
 // TODO: propper styling of the avatars
 const avatar = (assignee) =>
 `
-<span class="aui-avatar">
+<span class="aui-avatar jira-swimlane-extension">
   <span class="aui-avatar-inner">
     <img src="${assignee.avatarUrls["48x48"]}"
          class="aui-avatar-img"
          title="Bearbeiter: ${assignee.displayName}"
          loading="lazy"
          alt="${assignee.name}"
-     />
+    />
   </span>
 </span>
 `
@@ -34,7 +43,7 @@ const avatar = (assignee) =>
 const displayAssignee = (element, json) => {
     let assignee = json.fields.assignee
     if(assignee == null) return;
-    let e = element.getElementsByClassName("ghx-bandaid")[0];
+    let e = element.querySelector(".ghx-bandaid");
     e.innerHTML = e.innerHTML + avatar(assignee);
 }
 
@@ -47,12 +56,26 @@ const loadTicketAndModifySwimlane = async (element) => {
 }
 
 const performJiraExtension = () => {
-    let y = window.document.getElementsByClassName("ghx-swimlane-header");
-    Array.prototype.forEach.call(y, element => {
-        loadTicketAndModifySwimlane(element);
-    });
+    Array.prototype.forEach.call(
+        window.document.getElementsByClassName("ghx-swimlane-header"),
+        element => loadTicketAndModifySwimlane(element));
 };
 
+const elementIsRemoved = async (selector) => {
+    while (document.querySelector(selector) !== null) {
+        await new Promise((r) => setTimeout(r, 2000));
+    }
+    return true;
+}
+
+const jiraExtension = async () => {
+    while (true) {
+        let isRemoved = await elementIsRemoved(".jira-swimlane-extension");
+        if (isRemoved) performJiraExtension();
+        await new Promise((r) => setTimeout(r, 5000));
+    }
+}
+
 (function() {
-    setTimeout(performJiraExtension, 5000);
+    setTimeout(() => jiraExtension(), 2000);
 })();
